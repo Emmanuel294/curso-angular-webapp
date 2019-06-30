ee84bbc1b87ac23280c512dad8e835b8dbf6589d
Emmanuel Cobian <emmanuelcobian@la-MacBook-Pro-de-Emmanuel-225.local>
2019-06-30 00:31:21 -0500

    fichero de configuracion global

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GLOBAL } from './services/global';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Productos Angular';
+  public headColor:string;
+
+  constructor(){
+    this.headColor = GLOBAL.header_color;
+  }
 }
---
+++ src/app/services/global.ts
@@ -0,0 +1,4 @@
+export var GLOBAL = {
+    url: 'http://localhost:8888/curso-angular-backend/index.php/',
+    header_color: '#E03137'
+};
