467751bef0559a0a012963255ce68bb4e38453c3
Emmanuel Cobian <emmanuelcobian@la-MacBook-Pro-de-Emmanuel-225.local>
2019-06-29 23:37:47 -0500

    home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
+//Componentes
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
